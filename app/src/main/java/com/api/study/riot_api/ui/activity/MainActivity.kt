dc44c5e04ab7a1664b8c7b40acdcc844d054658b
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-04-27 18:25:43 +0900

    :construction: databinding, viewModel connection

---
+++ app/src/main/java/com/api/study/riot_api/ui/activity/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.api.study.riot_api.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.api.study.riot_api.R
+import com.api.study.riot_api.databinding.ActivityLoginBinding
+import com.api.study.riot_api.databinding.ActivityMainBinding
+import com.api.study.riot_api.viewModel.Login_ViewModel
+import com.api.study.riot_api.viewModel.Main_ViewModel
+
+class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy { DataBindingUtil.setContentView(this, R.layout.activity_main) }
+    private val viewModel: Main_ViewModel by lazy { ViewModelProvider(this)[Main_ViewModel::class.java] }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding.main = viewModel
+        binding.lifecycleOwner = this
+    }
+}
