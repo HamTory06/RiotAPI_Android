c95b8539615ee60b44616ca82560b3d95a0cba27
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-05-05 02:29:12 +0900

    :test_tube: Data disappears when moving Model -> viewModel

--- app/src/main/java/com/api/study/riot_api/viewModel/Main_ViewModel.kt
+++ app/src/main/java/com/api/study/riot_api/viewModel/Main_ViewModel.kt
@@ -1,7 +1,26 @@
 package com.api.study.riot_api.viewModel
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.api.study.riot_api.data.network.retrofit.Communication
+import com.api.study.riot_api.data.network.retrofit.LoggingInterceptor
+import com.api.study.riot_api.data.network.retrofit.response.User_Information_response
 
 class Main_ViewModel: ViewModel() {
+    private val repository = Communication()
 
+
+    private val _userName = MutableLiveData<String>()
+    val userName: LiveData<String>
+        get() = _userName
+
+    fun setInputUserName(text: String) {
+        _userName.value = text
+        if(!_userName.value.isNullOrEmpty()){
+            val userData: LiveData<User_Information_response> = repository.GetUserInformation(userName.value!!)
+            Log.d("상태", userData.value.toString())
+        }
+    }
 }
