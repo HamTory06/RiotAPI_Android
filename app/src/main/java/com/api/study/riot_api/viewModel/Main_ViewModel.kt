a4df2361cdf62407300ccb591b11c9aceeaea689
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-05-05 16:59:51 +0900

    :card_file_box: Successfully receiving data from Model

--- app/src/main/java/com/api/study/riot_api/viewModel/Main_ViewModel.kt
+++ app/src/main/java/com/api/study/riot_api/viewModel/Main_ViewModel.kt
@@ -4,23 +4,34 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.api.study.riot_api.data.network.retrofit.ClientRetrofit
 import com.api.study.riot_api.data.network.retrofit.Communication
-import com.api.study.riot_api.data.network.retrofit.LoggingInterceptor
 import com.api.study.riot_api.data.network.retrofit.response.User_Information_response
+import com.api.study.riot_api.data.repository.RiotApiData
 
-class Main_ViewModel: ViewModel() {
+class Main_ViewModel() : ViewModel() {
     private val repository = Communication()
 
+    private val _userInformationData = MutableLiveData<User_Information_response>()
+    val userInformationData: LiveData<User_Information_response>
+        get() = _userInformationData
 
     private val _userName = MutableLiveData<String>()
     val userName: LiveData<String>
         get() = _userName
 
+    private val riotApiData = object : RiotApiData {
+        override fun onRiotAPI_GetUser_Information(data: User_Information_response) {
+            Log.d("상태", data.toString())
+            _userInformationData.value = data
+        }
+    }
+
     fun setInputUserName(text: String) {
         _userName.value = text
-        if(!_userName.value.isNullOrEmpty()){
-            val userData: LiveData<User_Information_response> = repository.GetUserInformation(userName.value!!)
-            Log.d("상태", userData.value.toString())
+
+        if (!_userName.value.isNullOrEmpty()) {
+            repository.GetUserInformation(text, riotApiData)
         }
     }
 }
