17965a54dba58079802259c5e756e2bc90764aa8
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-05-05 16:58:17 +0900

    :white_check_mark: Passing value to viewModel through retrofit communication succeeded

--- app/src/main/java/com/api/study/riot_api/data/network/retrofit/Communication.kt
+++ app/src/main/java/com/api/study/riot_api/data/network/retrofit/Communication.kt
@@ -4,51 +4,52 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.api.study.riot_api.data.network.retrofit.response.User_Information_response
+import com.api.study.riot_api.data.repository.RiotApiData
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 import okhttp3.Interceptor
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import kotlin.coroutines.CoroutineContext
 
 class Communication {
 
-    companion object{
+    companion object {
         const val api_key = "RGAPI-f9035c09-8617-4247-92e1-023c40f5b83b"
     }
 
-    fun GetUserInformation(username: String): MutableLiveData<User_Information_response>{
-        val data = MutableLiveData<User_Information_response>()
-        ClientRetrofit.api.get_user_information_name(username, api_key).enqueue(object : Callback<User_Information_response>{
-            override fun onResponse(
-                call: Call<User_Information_response>,
-                response: Response<User_Information_response>
-            ) {
-                if(response.isSuccessful){
-                    data.value = response.body()
-//                    Log.d("상태","${data.value}")
-                } else {
-                    Log.d("상태","response.code : ${response.code()}")
+    fun GetUserInformation(username: String, riotAPIData: RiotApiData) {
+        ClientRetrofit.api.get_user_information_name(username, api_key)
+            .enqueue(object : Callback<User_Information_response> {
+                override fun onResponse(
+                    call: Call<User_Information_response>,
+                    response: Response<User_Information_response>
+                ) {
+                    if (response.isSuccessful) {
+                        val data = response.body()!!
+                        riotAPIData.onRiotAPI_GetUser_Information(data)
+                    } else {
+                        Log.d("onResponse", "response.code : ${response.code()}")
+                    }
                 }
-            }
 
-            override fun onFailure(call: Call<User_Information_response>, t: Throwable) {
-                Log.d("상태","${t.message}")
-            }
-        })
-
-        return data
+                override fun onFailure(call: Call<User_Information_response>, t: Throwable) {
+                    Log.d("onFailure", "${t.message}")
+                }
+            })
     }
 }
 
 class LoggingInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): okhttp3.Response {
         val request = chain.request()
-
         // 최종 URL을 확인할 수 있습니다.
         val url = request.url()
-        Log.d("상태",url.toString())
-
+        Log.d("URL", url.toString())
         // 다른 작업을 수행할 수 있습니다.
-
         return chain.proceed(request)
     }
 }
