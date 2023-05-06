815186a4f3f5d77abe859fd1d87f1503eb62d6de
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-05-06 23:40:38 +0900

    :white_check_mark: retrofit check final url

---
+++ app/src/main/java/com/api/study/riot_api/data/network/retrofit/test/RetrofitInterceptor.kt
@@ -0,0 +1,15 @@
+package com.api.study.riot_api.data.network.retrofit.test
+
+import android.util.Log
+import okhttp3.Interceptor
+
+class LoggingInterceptor : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): okhttp3.Response {
+        val request = chain.request()
+        // 최종 URL을 확인할 수 있습니다.
+        val url = request.url()
+        Log.d("URL", url.toString())
+        // 다른 작업을 수행할 수 있습니다.
+        return chain.proceed(request)
+    }
+}
