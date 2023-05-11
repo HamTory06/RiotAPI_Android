ab3fb1bf98bbd171f94bce00ec79cf7c6701d9cb
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-05-11 23:53:33 +0900

    :construction: add recyclerview adapter

---
+++ app/src/main/java/com/api/study/riot_api/ui/activity/Main_RecyclerAdapter.kt
@@ -0,0 +1,22 @@
+package com.api.study.riot_api.ui.activity
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class Main_RecyclerAdapter: RecyclerView.Adapter<RecyclerAdapter.MyViewHolder>(){
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): RecyclerAdapter.MyViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: RecyclerAdapter.MyViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+}
