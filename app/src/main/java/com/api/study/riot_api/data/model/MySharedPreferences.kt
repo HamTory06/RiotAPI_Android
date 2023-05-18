8b69522a7bc5ada8fe364af4d310a26b36e70f1c
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-05-18 23:11:30 +0900

    :card_file_box: save roll version

--- app/src/main/java/com/api/study/riot_api/data/model/MySharedPreferences.kt
+++ app/src/main/java/com/api/study/riot_api/data/model/MySharedPreferences.kt
@@ -5,9 +5,14 @@ import android.content.SharedPreferences
 
 class MySharedPreferences(context: Context) {
     val PREFS_FILENAME = "prefs"
-    val PREF_KEY_MY_EDITTEXT = "myEditText"
+    val PREF_KEY_MY_PUUID = "puuid"
+    val PREF_KEY_MY_version = "version"
     val prefs: SharedPreferences = context.getSharedPreferences(PREFS_FILENAME, 0)
     var puuid: String?
-        get() = prefs.getString(PREF_KEY_MY_EDITTEXT, "")
-        set(value) = prefs.edit().putString(PREF_KEY_MY_EDITTEXT, value).apply()
+        get() = prefs.getString(PREF_KEY_MY_PUUID, "")
+        set(value) = prefs.edit().putString(PREF_KEY_MY_PUUID, value).apply()
+
+    var version: String?
+        get() = prefs.getString(PREF_KEY_MY_version,"")
+        set(value) = prefs.edit().putString(PREF_KEY_MY_version, value).apply()
 }
