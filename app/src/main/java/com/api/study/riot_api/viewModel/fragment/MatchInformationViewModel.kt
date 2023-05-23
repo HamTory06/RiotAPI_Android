4b07d1b0e5ea059dc86bc26630af45d6aaa766c3
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-05-23 18:08:56 +0900

    :construction: set ViewModel Match, User, Champion information

---
+++ app/src/main/java/com/api/study/riot_api/viewModel/fragment/MatchInformationViewModel.kt
@@ -0,0 +1,7 @@
+package com.api.study.riot_api.viewModel.fragment
+
+import androidx.lifecycle.ViewModel
+
+class MatchInformationViewModel: ViewModel() {
+
+}
---
+++ app/src/main/java/com/api/study/riot_api/viewModel/fragment/UserInformationViewModel.kt
@@ -0,0 +1,6 @@
+package com.api.study.riot_api.viewModel.fragment
+
+import androidx.lifecycle.ViewModel
+
+class UserInformationViewModel: ViewModel() {
+}
