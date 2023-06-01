539eaaba0b057b4e2f2a1af236ba9cee06835220
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-06-01 16:47:31 +0900

    :construction: server communication interface Query(apikey)

--- app/src/main/java/com/api/study/riot_api/data/network/retrofit/riot/RiotApi.kt
+++ app/src/main/java/com/api/study/riot_api/data/network/retrofit/riot/RiotApi.kt
@@ -4,20 +4,23 @@ import com.api.study.riot_api.data.network.retrofit.riot.response.RiotUserPuuidR
 import com.api.study.riot_api.data.network.retrofit.riot.response.RiotVersionsResponse
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface RiotApi {
-    @GET("/api/versions.json")
-    suspend fun get_lol_versions(): RiotVersionsResponse
+
 
     @GET("/riot/account/v1/accounts/by-riot-id/{gameName}/{tagLine}")
     suspend fun get_user_puuid(
         @Path(value = "gameName") gameName: String,
-        @Path(value = "tagLine") tagLine: String
+        @Path(value = "tagLine") tagLine: String,
+        @Query(value = "api_key") api_key: String
+
     ): RiotUserPuuidResponse
 
     @GET("/riot/account/v1/accounts/by-puuid/{puuid}")
     suspend fun get_user_puuid(
-        @Path(value = "puuid") puuid: String
+        @Path(value = "puuid") puuid: String,
+        @Query(value = "api_key") api_key: String
     ): RiotUserPuuidResponse
 
 
