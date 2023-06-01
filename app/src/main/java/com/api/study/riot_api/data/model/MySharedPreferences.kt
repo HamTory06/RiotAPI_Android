8068e7a0918019449f11ec5e7157652aceac9d74
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-06-01 16:49:30 +0900

    :card_file_box: Modify SharedPreferences

--- app/src/main/java/com/api/study/riot_api/data/model/MySharedPreferences.kt
+++ app/src/main/java/com/api/study/riot_api/data/model/MySharedPreferences.kt
@@ -4,15 +4,33 @@ import android.content.Context
 import android.content.SharedPreferences
 
 class MySharedPreferences(context: Context) {
-    val PREFS_FILENAME = "prefs"
-    val PREF_KEY_MY_PUUID = "puuid"
-    val PREF_KEY_MY_version = "version"
-    val prefs: SharedPreferences = context.getSharedPreferences(PREFS_FILENAME, 0)
-    var puuid: String?
-        get() = prefs.getString(PREF_KEY_MY_PUUID, "")
-        set(value) = prefs.edit().putString(PREF_KEY_MY_PUUID, value).apply()
+    companion object {
+        const val PREFS_FILENAME = "prefs"
+        const val PREF_KEY_MY_LOL_PUUID = "lolpuuid"
+        const val PREF_KEY_MY_LOL_VERSION = "lolversion"
+        const val PREF_KEY_MY_VAL_PUUID = "valpuuid"
+        const val PREF_KEY_MY_VAL_VERSION = "valversion"
+        const val PREF_KEY_MY_LOCALE = "locale"
+    }
 
-    var version: String?
-        get() = prefs.getString(PREF_KEY_MY_version,"")
-        set(value) = prefs.edit().putString(PREF_KEY_MY_version, value).apply()
+    private val prefs: SharedPreferences = context.getSharedPreferences(PREFS_FILENAME, 0)
+    var lolpuuid: String?
+        get() = prefs.getString(PREF_KEY_MY_LOL_PUUID, "")
+        set(value) = prefs.edit().putString(PREF_KEY_MY_LOL_PUUID, value).apply()
+
+    var lolVersion: String?
+        get() = prefs.getString(PREF_KEY_MY_LOL_VERSION,"")
+        set(value) = prefs.edit().putString(PREF_KEY_MY_LOL_VERSION, value).apply()
+
+    var valpuuid: String?
+        get() = prefs.getString(PREF_KEY_MY_VAL_PUUID, "")
+        set(value) = prefs.edit().putString(PREF_KEY_MY_VAL_PUUID, value).apply()
+
+    var ValVersion: String?
+        get() = prefs.getString(PREF_KEY_MY_LOL_VERSION,"")
+        set(value) = prefs.edit().putString(PREF_KEY_MY_LOL_VERSION, value).apply()
+
+    var locale: String?
+        get() = prefs.getString(PREF_KEY_MY_LOCALE, "")
+        set(value) = prefs.edit().putString(PREF_KEY_MY_LOL_VERSION, value).apply()
 }
