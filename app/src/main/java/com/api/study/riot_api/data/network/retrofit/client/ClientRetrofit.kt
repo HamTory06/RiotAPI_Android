1ba09c0015229b4fba0f5665852d01ef14a8f0a3
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-06-01 16:54:32 +0900

    :construction: change variable name

--- app/src/main/java/com/api/study/riot_api/data/network/retrofit/client/ClientRetrofit.kt
+++ app/src/main/java/com/api/study/riot_api/data/network/retrofit/client/ClientRetrofit.kt
@@ -1,16 +1,14 @@
-package com.api.study.riot_api.data.network.retrofit
+package com.api.study.riot_api.data.network.retrofit.client
 
-import com.api.study.riot_api.data.network.retrofit.test.LoggingInterceptor
+import com.api.study.riot_api.data.network.retrofit.lol.LOLapi
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.create
 
 object ClientRetrofit {
     const val krBaseURL = "https://kr.api.riotgames.com/"
     const val asiarBaseURL = "https://asia.api.riotgames.com/"
     const val ddragonBaseURL = "https://ddragon.leagueoflegends.com/"
-
 
     val loggingInterceptor = LoggingInterceptor()
 
@@ -20,18 +18,17 @@ object ClientRetrofit {
 
     val krRetrofit = Retrofit.Builder()
         .baseUrl(krBaseURL)
-        .client(client)
+//        .client(client)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
-    val krApi: API = krRetrofit.create(API::class.java)
-
+    val krApi: LOLapi = krRetrofit.create(LOLapi::class.java)
 
     val ddragonRetrofit = Retrofit.Builder()
         .baseUrl(ddragonBaseURL)
-        .client(client)
+//        .client(client)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
-    val ddragonApi: API = ddragonRetrofit.create(API::class.java)
+    val ddragonApi: LOLapi = ddragonRetrofit.create(LOLapi::class.java)
 
 
     val asiaretrofit = Retrofit.Builder()
@@ -39,7 +36,7 @@ object ClientRetrofit {
         .client(client)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
-    val asiaApi: API = asiaretrofit.create(API::class.java)
+    val asiaLOLAPI: LOLapi = asiaretrofit.create(LOLapi::class.java)
 
 
     fun krGetInstance() : Retrofit {
