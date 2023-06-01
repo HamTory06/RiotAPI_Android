85b4d228c30f1e7d809c4b969330eb79fceb1808
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-06-01 16:47:03 +0900

    :construction: Add server communication interface

--- app/src/main/java/com/api/study/riot_api/data/network/retrofit/val/VALORANTapi.kt
+++ app/src/main/java/com/api/study/riot_api/data/network/retrofit/val/VALORANTapi.kt
@@ -4,23 +4,36 @@ import com.api.study.riot_api.data.network.retrofit.`val`.response.valStatusResp
 import com.api.study.riot_api.data.network.retrofit.`val`.response.valrankedResponse.valRankedesponse
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface VALORANTapi {
 
     @GET("/val/status/v1/platform-data")
-    suspend fun get_val_status(): ValStatusResponse
-
-    @GET("/val/ranked/v1/leaderboards/by-act/{actId}")
-    suspend fun get_val_ranked(
-        @Path(value = "actId") actId: String
-    )
+    suspend fun get_val_status(
+        @Query(value = "api_key") api_key: String
+    ): ValStatusResponse
 
     @GET("/val/content/v1/contents")
-    suspend fun get_val_contents(): ValStatusResponse
+    suspend fun get_val_contents(
+        @Query(value = "api_key") api_key: String
+    ): ValStatusResponse
 
     @GET("/val/ranked/v1/leaderboards/by-act/{actId}")
     suspend fun get_ranked(
-        @Path(value = "actId") actId:String
+        @Path(value = "actId") actId: String,
+        @Query(value = "api_key") api_key: String
     ): valRankedesponse
+
+    @GET("/val/match/v1/matches/{matchId}")
+    suspend fun get_match(
+        @Path(value = "matchId") matchId: String,
+        @Query(value = "api_key") api_key: String
+    )
+
+    @GET("/val/match/v1/matchlists/by-puuid/{puuid}")
+    suspend fun get_matchId(
+        @Path(value = "puuid") puuid: String,
+        @Query(value = "api_key") api_key: String
+    )
 
 }
