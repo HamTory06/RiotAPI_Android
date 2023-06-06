1ebd289169d6f0a864cbc921ab6c4e0af5d4db22
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-06-07 01:25:50 +0800

    :truck: class name change

--- app/src/main/java/com/api/study/riot_api/data/network/riotapi/RiotAPI.kt
+++ app/src/main/java/com/api/study/riot_api/data/network/riotapi/RiotAPI.kt
@@ -1,8 +1,8 @@
 package com.api.study.riot_api.data.network.riotapi
 
 import android.widget.ImageView
+import com.api.study.riot_api.ui.activity.LOLBaseActivity
 
-import com.api.study.riot_api.ui.activity.LOLStatsSearchActivity
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.bumptech.glide.request.RequestOptions
@@ -13,20 +13,20 @@ class RiotAPI {
         .transform(RoundedCorners(10))
 
     fun getImageChampionProfileIcon(binding: ImageView, ID: String, version: String) {
-        Glide.with(LOLStatsSearchActivity.ApplicationContext())
+        Glide.with(LOLBaseActivity.ApplicationContext())
             .load("https://ddragon.leagueoflegends.com/cdn/${version}/img/champion/${ID}.png")
             .into(binding)
     }
 
     fun getImageChampionSpell(binding: ImageView, ID: String, version: String) {
-        Glide.with(LOLStatsSearchActivity.ApplicationContext())
+        Glide.with(LOLBaseActivity.ApplicationContext())
             .load("https://ddragon.leagueoflegends.com/cdn/${version}/img/spell/${ID}.png")
             .apply(requestOptions)
             .into(binding)
     }
 
     fun getImageChampionItem(binding: ImageView, ID: String, version: String) {
-        Glide.with(LOLStatsSearchActivity.ApplicationContext())
+        Glide.with(LOLBaseActivity.ApplicationContext())
             .load("https://ddragon.leagueoflegends.com/cdn/${version}/img/item/${ID}.png")
             .apply(requestOptions)
             .into(binding)
