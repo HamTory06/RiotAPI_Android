ea42c5f41fadc1496d2f6cd86d127982acb16889
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-06-11 23:44:36 +0900

    :construction: Get LOL match information under ViewModel creation

---
+++ app/src/main/java/com/api/study/riot_api/ui/fragment/LOLStatsSearchFragment.kt
@@ -0,0 +1,77 @@
+package com.api.study.riot_api.ui.fragment
+
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.api.study.riot_api.OnItemClickListener
+import com.api.study.riot_api.R
+import com.api.study.riot_api.databinding.FragmentLolStatsSearchBinding
+import com.api.study.riot_api.ui.activity.LOLBaseActivity
+import com.api.study.riot_api.ui.adapter.LOLRecyclerAdapter
+import com.api.study.riot_api.viewModel.fragment.LOLStatsSearchViewModel
+
+
+class LOLStatsSearchFragment : Fragment() {
+
+
+    lateinit var binding: FragmentLolStatsSearchBinding
+
+    private val viewModel by lazy {
+        ViewModelProvider(this)[LOLStatsSearchViewModel::class.java]
+    }
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentLolStatsSearchBinding.inflate(inflater, container, false)
+        binding.main = viewModel
+        binding.lifecycleOwner = this
+        binding.LOLRecyclerView.layoutManager =
+            LinearLayoutManager(LOLBaseActivity.ApplicationContext())
+        return binding.root
+    }
+
+    override fun onResume() {
+        super.onResume()
+        viewModel.recycler.observe(this, Observer {
+            var newAdapter = LOLRecyclerAdapter(
+                it,
+                LOLBaseActivity.ApplicationContext(),
+                object : OnItemClickListener {
+                    override fun onItemClick(item: Int) {
+                        val bundle = Bundle()
+                        bundle.putInt("position",item)
+                        findNavController().navigate(R.id.action_LOLStatsSearchFragment_to_matchInformationFragment,bundle)
+                    }
+                }
+            )
+            binding.LOLRecyclerView.adapter = newAdapter
+        })
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.main = viewModel
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+}
---
+++ app/src/main/java/com/api/study/riot_api/viewModel/fragment/LOLStatsSearchViewModel.kt
@@ -0,0 +1,144 @@
+package com.api.study.riot_api.viewModel.fragment
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.navigation.fragment.findNavController
+import com.api.study.riot_api.R
+import com.api.study.riot_api.data.model.MySharedPreferences
+import com.api.study.riot_api.data.network.retrofit.client.ClientRetrofit
+import com.api.study.riot_api.data.network.retrofit.lol.LOLapi
+import com.api.study.riot_api.data.network.retrofit.lol.response.UserInformationResponse
+import com.api.study.riot_api.data.network.retrofit.lol.response.UserMatchesIdResponse
+import com.api.study.riot_api.data.network.retrofit.lol.response.user_matches_response.UserMatchesResponse
+import com.api.study.riot_api.data.network.retrofit.riot.response.RiotUserPuuidResponse
+import com.api.study.riot_api.data.network.retrofit.riot.response.RiotVersionsResponse
+import com.api.study.riot_api.ui.activity.LOLBaseActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import java.lang.Exception
+import java.time.Duration
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneId
+
+class LOLStatsSearchViewModel : ViewModel() {
+    companion object {
+        const val API_KEY = "RGAPI-f9035c09-8617-4247-92e1-023c40f5b83b"
+    }
+
+    private var krRetrofitInstance: LOLapi =
+        ClientRetrofit.krGetInstance().create(LOLapi::class.java)
+    private var asiarRetrofitInstance: LOLapi =
+        ClientRetrofit.AsiarGetInstance().create(LOLapi::class.java)
+    private var ddragonGetInstance: LOLapi =
+        ClientRetrofit.ddragonGetInstance().create(LOLapi::class.java)
+
+
+    private val viewModelScope = CoroutineScope(Dispatchers.Main)
+
+    private val _userInformationData = MutableLiveData<UserInformationResponse>()
+    val userInformationData: LiveData<UserInformationResponse>
+        get() = _userInformationData
+
+    private val _userMatchesId = MutableLiveData<UserMatchesIdResponse>()
+    val userMatchId: LiveData<UserMatchesIdResponse>
+        get() = _userMatchesId
+
+    private val _userMatches = MutableLiveData<List<UserMatchesResponse>>()
+    val userMatches: LiveData<List<UserMatchesResponse>>
+        get() = _userMatches
+
+    private val _userName = MutableLiveData<String>()
+    val userName: LiveData<String>
+        get() = _userName
+
+    private val _recyclerView = MutableLiveData<ArrayList<UserMatchesResponse>>()
+    val recycler: LiveData<ArrayList<UserMatchesResponse>>
+        get() = _recyclerView
+
+
+    private fun getVersions() {
+        var data: RiotVersionsResponse
+        CoroutineScope(Dispatchers.IO).async {
+            data = ddragonGetInstance.get_lol_versions()
+            MySharedPreferences(LOLBaseActivity.ApplicationContext()).lolVersion = data[0]
+        }
+    }
+
+    private fun getPuuid(name: String){
+        var data: UserInformationResponse
+        CoroutineScope(Dispatchers.IO).async {
+            data = krRetrofitInstance.get_user_information_name(name, API_KEY)
+            MySharedPreferences(LOLBaseActivity.ApplicationContext()).lolpuuid = data.puuid
+        }
+    }
+
+    fun setInputUserName(text: String) = viewModelScope.launch {
+        CoroutineScope(Dispatchers.IO).async {
+            getVersions()
+            getPuuid(text)
+            Log.d("상태",MySharedPreferences(LOLBaseActivity.ApplicationContext()).lolVersion.toString())
+            val data = ArrayList<UserMatchesResponse>()
+            _userName.postValue(text)
+            val userMatchesList = mutableListOf<UserMatchesResponse>()
+            val puuid = MySharedPreferences(LOLBaseActivity.ApplicationContext()).lolpuuid
+            Log.d(
+                "puuId", MySharedPreferences(LOLBaseActivity.ApplicationContext()).lolpuuid.toString()
+            )
+            val userMatchesId = async {
+                asiarRetrofitInstance.get_user_matchesId(
+                    puuid?:"", API_KEY, 0, 10
+                )
+            }
+            userMatchesList.clear()
+            for (userMatchesId in userMatchesId.await()) {
+                try {
+                    val userMatches = async {
+                        asiarRetrofitInstance.get_user_matches(
+                            userMatchesId, API_KEY
+                        )
+                    }
+                    data.add(userMatches.await())
+                } catch (e: Exception) {
+                    Log.d("ERROR", e.message.toString())
+                }
+            }
+            _recyclerView.postValue(data)
+        }
+    }
+
+    fun getTimeAfterGameOver(gameEndTimestamp: Long): String {
+        val endDateTime =
+            LocalDateTime.ofInstant(Instant.ofEpochMilli(gameEndTimestamp), ZoneId.systemDefault())
+
+        // 현재 시간 얻기
+        val currentDateTime = LocalDateTime.now()
+
+        // 게임 종료 시간과 현재 시간의 차이 계산
+        val duration = Duration.between(endDateTime, currentDateTime)
+
+        val days = duration.toDays()
+        val hours = duration.toHours() % 24
+        val minutes = duration.toMinutes() % 60
+
+        return if (days == 0L && hours == 0L) {
+            "${minutes.toInt()}분전"
+        } else if (days == 0L) {
+            if (minutes >= 50) {
+                "${hours.toInt() + 1}시간전"
+            } else {
+                "${hours.toInt()}시간전"
+            }
+        } else {
+            "${days.toInt()}일전"
+        }
+    }
+
+    fun nextFragment() {
+
+    }
+}
