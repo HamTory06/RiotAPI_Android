f180152c81883fa9aa330b50aee7da7faa3348d6
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-07-03 23:49:41 +0900

    :zap: ID duplicate check logic

--- app/src/main/java/com/api/study/riot_api/ui/fragment/account/SignupFragment.kt
+++ app/src/main/java/com/api/study/riot_api/ui/fragment/account/SignupFragment.kt
@@ -10,6 +10,7 @@ import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.api.study.riot_api.R
+import com.api.study.riot_api.data.model.dto.CheckSameIdDto
 import com.api.study.riot_api.data.model.dto.signupDto.request.SignupRequestDto
 import com.api.study.riot_api.data.network.retrofit.client.ClientRetrofit
 import com.api.study.riot_api.databinding.FragmentSignupBinding
@@ -31,6 +32,12 @@ class SignupFragment : Fragment() {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_signup, container, false)
         binding.signup = signupViewModel // LoginViewModel을 바인딩
         binding.lifecycleOwner = this // lifecycleOwner 설정
+
+        signupViewModel.sameIdCheckButtonStatus.observe(viewLifecycleOwner, Observer {
+            Log.d("클릭","클릭")
+
+            click(binding.idEdittext.text.toString())
+        })
 
         signupViewModel.signupButtonStatus.observe(viewLifecycleOwner, Observer {
             signup(id = binding.idEdittext.text.toString(), name = binding.nameEdittext1.text.toString(), password = binding.passwordEdittext1.text.toString())
@@ -55,6 +62,29 @@ class SignupFragment : Fragment() {
 
             override fun onFailure(call: Call<SignupRequestDto>, t: Throwable) {
                 Log.d("애러","${t.message}")
+            }
+
+        })
+    }
+
+    private fun click(id: String){
+        ClientRetrofit.api.checkSameId(id).enqueue(object : Callback<CheckSameIdDto>{
+            override fun onResponse(
+                call: Call<CheckSameIdDto>,
+                response: Response<CheckSameIdDto>
+            ) {
+                if(response.isSuccessful){
+                    Log.d("성공",response.body().toString())
+                    if(response.body()?.sameId!!){
+                        //TODO(true면 id edittext background 빨간색,글씨도 빨간색으로 변경하고 id edittext 밑에 "이미 존재하는 아이디 입니다" textview로 보여주기)
+                    }
+                } else{
+                    Log.d("실패",response.code().toString())
+                }
+            }
+
+            override fun onFailure(call: Call<CheckSameIdDto>, t: Throwable) {
+
             }
 
         })
