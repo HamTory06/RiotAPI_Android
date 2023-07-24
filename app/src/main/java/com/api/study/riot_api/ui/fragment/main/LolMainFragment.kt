6ccb880fb93ce4712e442bfb3755d8089af8597a
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-07-25 00:23:50 +0900

    :sparkles: Logic to find lol user information

--- app/src/main/java/com/api/study/riot_api/ui/fragment/main/LolMainFragment.kt
+++ app/src/main/java/com/api/study/riot_api/ui/fragment/main/LolMainFragment.kt
@@ -1,6 +1,7 @@
 package com.api.study.riot_api.ui.fragment.main
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -11,10 +12,13 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.api.study.riot_api.R
+import com.api.study.riot_api.data.model.dto.LolUserDto
+import com.api.study.riot_api.data.network.retrofit.client.ClientRetrofit
 import com.api.study.riot_api.databinding.FragmentLolMainBinding
-import com.api.study.riot_api.databinding.FragmentMainScreenBinding
 import com.api.study.riot_api.viewModel.fragment.main.LolMainViewModel
-import com.api.study.riot_api.viewModel.fragment.main.MainScreenViewModel
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 
 class LolMainFragment : Fragment() {
@@ -30,20 +34,47 @@ class LolMainFragment : Fragment() {
         binding.lol = viewModel
         binding.lifecycleOwner = this
         requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner) {
-            onBackButtonClicked()
+            backFragment()
         }
 
         viewModel.backButtonStatus.observe(viewLifecycleOwner, Observer{
             if(it){
-                onBackButtonClicked()
+                backFragment()
+            }
+        })
+
+        viewModel.searchButtonStatus.observe(viewLifecycleOwner, Observer {
+            if(it){
+                Log.d("상태","클릭")
+                val name = binding.searchEdittext.text.toString()
+                search(name)
             }
         })
 
         return binding.root
     }
-    private fun onBackButtonClicked() {
+    private fun backFragment() {
         val action = LolMainFragmentDirections.actionLolMainFragmentToMainScreenFragment()
         findNavController().navigate(action)
+    }
+
+    private fun search(name: String){
+        ClientRetrofit.api.findUser(name).enqueue(object : Callback<LolUserDto>{
+            override fun onResponse(call: Call<LolUserDto>, response: Response<LolUserDto>) {
+                if(response.isSuccessful){
+                    Log.d("상태",response.body().toString())
+
+                } else {
+                    Log.d("상태",response.code().toString())
+
+                }
+            }
+
+            override fun onFailure(call: Call<LolUserDto>, t: Throwable) {
+
+            }
+
+        })
     }
 
 }
--- app/src/main/java/com/api/study/riot_api/viewModel/fragment/main/LolMainViewModel.kt
+++ app/src/main/java/com/api/study/riot_api/viewModel/fragment/main/LolMainViewModel.kt
@@ -8,9 +8,17 @@ class LolMainViewModel: ViewModel() {
     private val _backButtonStatus = MutableLiveData<Boolean>()
     val backButtonStatus: LiveData<Boolean> = _backButtonStatus
 
+    private val _searchButtonStatus = MutableLiveData<Boolean>()
+    val searchButtonStatus: LiveData<Boolean> = _searchButtonStatus
+
 
     fun onclickBackButton(){
         _backButtonStatus.value = true
     }
+
+    fun onClickSearchButton(){
+        _searchButtonStatus.value = true
+    }
+
 
 }
